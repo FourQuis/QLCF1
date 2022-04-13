e9ee3d901ecf2ff1497bf71cb265aa7b64597b86
Lê Tứ Qúy <96305213+FourQuis@users.noreply.github.com>
2022-04-13 15:31:45 +0700

    first commit

---
+++ QLCF/QLCF/.Designer.cs
@@ -0,0 +1,420 @@
+﻿namespace QLCF
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.btthemmon = new System.Windows.Forms.Button();
+            this.cbbtenmon = new System.Windows.Forms.ComboBox();
+            this.cbbLoai = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.flowLayoutPanel3 = new System.Windows.Forms.FlowLayoutPanel();
+            this.btban1 = new System.Windows.Forms.Button();
+            this.btban2 = new System.Windows.Forms.Button();
+            this.btban3 = new System.Windows.Forms.Button();
+            this.btban4 = new System.Windows.Forms.Button();
+            this.btban5 = new System.Windows.Forms.Button();
+            this.btban6 = new System.Windows.Forms.Button();
+            this.btban7 = new System.Windows.Forms.Button();
+            this.btban8 = new System.Windows.Forms.Button();
+            this.btban9 = new System.Windows.Forms.Button();
+            this.btban10 = new System.Windows.Forms.Button();
+            this.btban11 = new System.Windows.Forms.Button();
+            this.btban12 = new System.Windows.Forms.Button();
+            this.btban13 = new System.Windows.Forms.Button();
+            this.btban14 = new System.Windows.Forms.Button();
+            this.btban15 = new System.Windows.Forms.Button();
+            this.btmangve = new System.Windows.Forms.Button();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.panel1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            this.flowLayoutPanel3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(991, 555);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.panel2);
+            this.tabPage1.Controls.Add(this.panel1);
+            this.tabPage1.Controls.Add(this.flowLayoutPanel3);
+            this.tabPage1.Location = new System.Drawing.Point(4, 25);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(983, 526);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Oder";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.dataGridView1);
+            this.panel2.Location = new System.Drawing.Point(490, 157);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(453, 267);
+            this.panel2.TabIndex = 5;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 3);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(447, 261);
+            this.dataGridView1.TabIndex = 0;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.numericUpDown1);
+            this.panel1.Controls.Add(this.btthemmon);
+            this.panel1.Controls.Add(this.cbbtenmon);
+            this.panel1.Controls.Add(this.cbbLoai);
+            this.panel1.Controls.Add(this.label2);
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Location = new System.Drawing.Point(490, 18);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(453, 123);
+            this.panel1.TabIndex = 4;
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Location = new System.Drawing.Point(381, 36);
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(52, 23);
+            this.numericUpDown1.TabIndex = 3;
+            // 
+            // btthemmon
+            // 
+            this.btthemmon.Location = new System.Drawing.Point(278, 30);
+            this.btthemmon.Name = "btthemmon";
+            this.btthemmon.Size = new System.Drawing.Size(84, 32);
+            this.btthemmon.TabIndex = 2;
+            this.btthemmon.Text = "Thêm món";
+            this.btthemmon.UseVisualStyleBackColor = true;
+            this.btthemmon.Click += new System.EventHandler(this.btthemmon_Click);
+            // 
+            // cbbtenmon
+            // 
+            this.cbbtenmon.FormattingEnabled = true;
+            this.cbbtenmon.Location = new System.Drawing.Point(78, 59);
+            this.cbbtenmon.Name = "cbbtenmon";
+            this.cbbtenmon.Size = new System.Drawing.Size(178, 24);
+            this.cbbtenmon.TabIndex = 1;
+            // 
+            // cbbLoai
+            // 
+            this.cbbLoai.FormattingEnabled = true;
+            this.cbbLoai.Items.AddRange(new object[] {
+            "Thức ăn",
+            "Đồ uống"});
+            this.cbbLoai.Location = new System.Drawing.Point(78, 12);
+            this.cbbLoai.Name = "cbbLoai";
+            this.cbbLoai.Size = new System.Drawing.Size(178, 24);
+            this.cbbLoai.TabIndex = 1;
+            this.cbbLoai.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.Location = new System.Drawing.Point(17, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 30);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Tên";
+            // 
+            // label1
+            // 
+            this.label1.Location = new System.Drawing.Point(17, 15);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(73, 30);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Loại";
+            // 
+            // flowLayoutPanel3
+            // 
+            this.flowLayoutPanel3.Controls.Add(this.btban1);
+            this.flowLayoutPanel3.Controls.Add(this.btban2);
+            this.flowLayoutPanel3.Controls.Add(this.btban3);
+            this.flowLayoutPanel3.Controls.Add(this.btban4);
+            this.flowLayoutPanel3.Controls.Add(this.btban5);
+            this.flowLayoutPanel3.Controls.Add(this.btban6);
+            this.flowLayoutPanel3.Controls.Add(this.btban7);
+            this.flowLayoutPanel3.Controls.Add(this.btban8);
+            this.flowLayoutPanel3.Controls.Add(this.btban9);
+            this.flowLayoutPanel3.Controls.Add(this.btban10);
+            this.flowLayoutPanel3.Controls.Add(this.btban11);
+            this.flowLayoutPanel3.Controls.Add(this.btban12);
+            this.flowLayoutPanel3.Controls.Add(this.btban13);
+            this.flowLayoutPanel3.Controls.Add(this.btban14);
+            this.flowLayoutPanel3.Controls.Add(this.btban15);
+            this.flowLayoutPanel3.Controls.Add(this.btmangve);
+            this.flowLayoutPanel3.Location = new System.Drawing.Point(8, 15);
+            this.flowLayoutPanel3.Name = "flowLayoutPanel3";
+            this.flowLayoutPanel3.Size = new System.Drawing.Size(432, 409);
+            this.flowLayoutPanel3.TabIndex = 3;
+            // 
+            // btban1
+            // 
+            this.btban1.Location = new System.Drawing.Point(3, 3);
+            this.btban1.Name = "btban1";
+            this.btban1.Size = new System.Drawing.Size(98, 92);
+            this.btban1.TabIndex = 0;
+            this.btban1.Text = "Bàn 1";
+            this.btban1.UseVisualStyleBackColor = true;
+            // 
+            // btban2
+            // 
+            this.btban2.Location = new System.Drawing.Point(107, 3);
+            this.btban2.Name = "btban2";
+            this.btban2.Size = new System.Drawing.Size(98, 92);
+            this.btban2.TabIndex = 0;
+            this.btban2.Text = "Bàn 2";
+            this.btban2.UseVisualStyleBackColor = true;
+            // 
+            // btban3
+            // 
+            this.btban3.Location = new System.Drawing.Point(211, 3);
+            this.btban3.Name = "btban3";
+            this.btban3.Size = new System.Drawing.Size(98, 92);
+            this.btban3.TabIndex = 0;
+            this.btban3.Text = "Bàn 3";
+            this.btban3.UseVisualStyleBackColor = true;
+            // 
+            // btban4
+            // 
+            this.btban4.Location = new System.Drawing.Point(315, 3);
+            this.btban4.Name = "btban4";
+            this.btban4.Size = new System.Drawing.Size(98, 92);
+            this.btban4.TabIndex = 0;
+            this.btban4.Text = "Bàn 4";
+            this.btban4.UseVisualStyleBackColor = true;
+            // 
+            // btban5
+            // 
+            this.btban5.Location = new System.Drawing.Point(3, 101);
+            this.btban5.Name = "btban5";
+            this.btban5.Size = new System.Drawing.Size(98, 92);
+            this.btban5.TabIndex = 0;
+            this.btban5.Text = "Bàn 5";
+            this.btban5.UseVisualStyleBackColor = true;
+            // 
+            // btban6
+            // 
+            this.btban6.Location = new System.Drawing.Point(107, 101);
+            this.btban6.Name = "btban6";
+            this.btban6.Size = new System.Drawing.Size(98, 92);
+            this.btban6.TabIndex = 0;
+            this.btban6.Text = "Bàn 6";
+            this.btban6.UseVisualStyleBackColor = true;
+            // 
+            // btban7
+            // 
+            this.btban7.Location = new System.Drawing.Point(211, 101);
+            this.btban7.Name = "btban7";
+            this.btban7.Size = new System.Drawing.Size(98, 92);
+            this.btban7.TabIndex = 0;
+            this.btban7.Text = "Bàn 7";
+            this.btban7.UseVisualStyleBackColor = true;
+            // 
+            // btban8
+            // 
+            this.btban8.Location = new System.Drawing.Point(315, 101);
+            this.btban8.Name = "btban8";
+            this.btban8.Size = new System.Drawing.Size(98, 92);
+            this.btban8.TabIndex = 0;
+            this.btban8.Text = "Bàn 8";
+            this.btban8.UseVisualStyleBackColor = true;
+            // 
+            // btban9
+            // 
+            this.btban9.Location = new System.Drawing.Point(3, 199);
+            this.btban9.Name = "btban9";
+            this.btban9.Size = new System.Drawing.Size(98, 92);
+            this.btban9.TabIndex = 0;
+            this.btban9.Text = "Bàn 9";
+            this.btban9.UseVisualStyleBackColor = true;
+            // 
+            // btban10
+            // 
+            this.btban10.Location = new System.Drawing.Point(107, 199);
+            this.btban10.Name = "btban10";
+            this.btban10.Size = new System.Drawing.Size(98, 92);
+            this.btban10.TabIndex = 0;
+            this.btban10.Text = "Bàn 10";
+            this.btban10.UseVisualStyleBackColor = true;
+            // 
+            // btban11
+            // 
+            this.btban11.Location = new System.Drawing.Point(211, 199);
+            this.btban11.Name = "btban11";
+            this.btban11.Size = new System.Drawing.Size(98, 92);
+            this.btban11.TabIndex = 0;
+            this.btban11.Text = "Bàn 11";
+            this.btban11.UseVisualStyleBackColor = true;
+            // 
+            // btban12
+            // 
+            this.btban12.Location = new System.Drawing.Point(315, 199);
+            this.btban12.Name = "btban12";
+            this.btban12.Size = new System.Drawing.Size(98, 92);
+            this.btban12.TabIndex = 0;
+            this.btban12.Text = "Bàn 12";
+            this.btban12.UseVisualStyleBackColor = true;
+            // 
+            // btban13
+            // 
+            this.btban13.Location = new System.Drawing.Point(3, 297);
+            this.btban13.Name = "btban13";
+            this.btban13.Size = new System.Drawing.Size(98, 92);
+            this.btban13.TabIndex = 0;
+            this.btban13.Text = "Bàn 13";
+            this.btban13.UseVisualStyleBackColor = true;
+            // 
+            // btban14
+            // 
+            this.btban14.Location = new System.Drawing.Point(107, 297);
+            this.btban14.Name = "btban14";
+            this.btban14.Size = new System.Drawing.Size(98, 92);
+            this.btban14.TabIndex = 0;
+            this.btban14.Text = "Bàn 14";
+            this.btban14.UseVisualStyleBackColor = true;
+            // 
+            // btban15
+            // 
+            this.btban15.Location = new System.Drawing.Point(211, 297);
+            this.btban15.Name = "btban15";
+            this.btban15.Size = new System.Drawing.Size(98, 92);
+            this.btban15.TabIndex = 0;
+            this.btban15.Text = "Bàn 15";
+            this.btban15.UseVisualStyleBackColor = true;
+            // 
+            // btmangve
+            // 
+            this.btmangve.Location = new System.Drawing.Point(315, 297);
+            this.btmangve.Name = "btmangve";
+            this.btmangve.Size = new System.Drawing.Size(98, 92);
+            this.btmangve.TabIndex = 0;
+            this.btmangve.Text = "Mang về";
+            this.btmangve.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 25);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(983, 526);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Món ăn";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Location = new System.Drawing.Point(4, 25);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(983, 526);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Thống kê";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(991, 555);
+            this.Controls.Add(this.tabControl1);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.Text = "QUẢN LÝ TÍNH TIỀN CAFE";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            this.flowLayoutPanel3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ComboBox cbbtenmon;
+        private System.Windows.Forms.ComboBox cbbLoai;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.FlowLayoutPanel flowLayoutPanel3;
+        private System.Windows.Forms.Button btban1;
+        private System.Windows.Forms.Button btban2;
+        private System.Windows.Forms.Button btban3;
+        private System.Windows.Forms.Button btban4;
+        private System.Windows.Forms.Button btban5;
+        private System.Windows.Forms.Button btban6;
+        private System.Windows.Forms.Button btban7;
+        private System.Windows.Forms.Button btban8;
+        private System.Windows.Forms.Button btban9;
+        private System.Windows.Forms.Button btban10;
+        private System.Windows.Forms.Button btban11;
+        private System.Windows.Forms.Button btban12;
+        private System.Windows.Forms.Button btban13;
+        private System.Windows.Forms.Button btban14;
+        private System.Windows.Forms.Button btban15;
+        private System.Windows.Forms.Button btmangve;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.Button btthemmon;
+    }
+}
+
---
+++ QLCF/QLCF/Quanlytinhtiencafe.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLCF
+{
+    public partial class Form1 : Form
+    {
+        DataTable data = new DataTable();
+        List<string> Doan = new List<string>(new string[] { "element1", "element2", "element3" });
+        List<string> Douong = new List<string>(new string[] { "element3", "element4", "element5" });
+        string tenban;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void showban()
+        {
+            data.Columns.AddRange(new DataColumn[] {
+                new DataColumn{ColumnName = "ID bàn" , DataType = typeof(int)},
+                new DataColumn{ColumnName ="Tên món",DataType =typeof(string)},
+                new DataColumn{ColumnName ="Số lượng",DataType=typeof(NumericUpDown)},
+                new DataColumn{ColumnName ="Giá",DataType=typeof (int)},
+            });
+
+        }
+        private void button_click(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+            tenban = button.Text;
+
+        }
+        
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+
+
+
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            cbbtenmon.Items.Clear();
+            if (cbbLoai.SelectedItem.ToString() == "Thức ăn")
+            {
+                cbbtenmon.Enabled = true;
+                foreach (string i in Doan)
+                {
+                    cbbtenmon.Items.Add(i);
+                }
+
+            }
+            else
+            {
+                cbbtenmon.Enabled = true;
+                foreach (string i in Douong)
+                {
+                    cbbtenmon.Items.Add(i);
+                }
+
+            }
+        }
+
+        private void btthemmon_Click(object sender, EventArgs e)
+        {
+            string tenban = this.tenban;
+            if (cbbLoai.SelectedIndex >= 0 && cbbtenmon.SelectedIndex >=0)
+            {
+                
+            }
+        }
+    }
+    
+}
